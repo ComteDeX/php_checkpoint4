e74c203b347865fd9470d660e8289be433ea9742
ComteDeX <comtedex@gmail.com>
2020-01-29 16:34:29 +0100

    Fixture User

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,8 +10,14 @@ class UserFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-//        $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        $user->setAddress1('23 rue des Canards');
+        $user->setZipCode('67000');
+        $user->setCity('STRASBOURG');
+        $user->setEmail('a@a.com');
+        $user->setPassword('$argon2id$v=19$m=65536,t=4,p=1$ADXC0RiTlDyLmnanGMFm8g$bDiElkSRoSodKXq57JrwTNb3I+0wUewT6gCKy8nLSic');
+        $user->setRoles('ROLE_ADMIN');
+        $manager->persist($user);
 
         $manager->flush();
     }
