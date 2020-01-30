49471365fbb294f146fa37103859a032d9576efe
ComteDeX <comtedex@gmail.com>
2020-01-30 22:32:28 +0100

    ajout du formulaire dans la homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\ContactFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -10,9 +13,13 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(Request $request): Response
     {
+        $form = $this->createForm(ContactFormType::class);
+        $form->handleRequest($request);
+
         return $this->render('home/index.html.twig', [
+            'form' => $form->createView(),
             'controller_name' => 'HomeController',
         ]);
     }
---
+++ src/Form/ContactFormType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $options;
+        $builder
+            ->add('name', null, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Votre nom'
+                ],
+            ])
+            ->add('e-mail', EmailType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Votre email'
+                    ],
+            ])
+            ->add('object', null, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Objet'
+                    ],
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Votre message'
+                    ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
